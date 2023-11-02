d2b67114c9527f4da5e1c303906438b1f604a33f
vanis <pavlik.vanis@email.cz>
2023-11-02 18:38:30 +0100

    + homepage responsivity

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="container min-h-screen items-center">
       <DateBox />
       <section id="home" className=" h-screen flex items-center justify-center">
-        <div className="flex flex-row items-end">
+        <div className="flex flex-col md:flex-row items-end">
           <Image
             width={100}
             height={50}
@@ -17,11 +17,11 @@ export default function Home() {
             alt="fe"
             className=" self-center  me-5"
           />
-          <h1 className=" min-h-[130px] text-center text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-700">
+          <h1 className=" md:min-h-[130px] text-center text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-700">
             Welcome Next.js
           </h1>
-          <div className="relative w-full h-full">
-            <div className=" absolute -bottom-4 left-1 h-14 w-full max-w-[250px] flex items-center justify-center bg-gradient-to-br from-purple-600 to-pink-500 rounded-3xl rotate-6">
+          <div className=" flex justify-center relative w-full">
+            <div className=" mt-8 md:-bottom-4 md:left-1 h-14 w-full max-w-[230px] flex items-center justify-center bg-gradient-to-br from-purple-600 to-pink-500 rounded-3xl rotate-3 md:rotate-6">
               <Image width={100} height={50} src="next.svg" alt="fe" />
             </div>
           </div>
